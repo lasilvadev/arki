80715b858fd80331cc200aad61ad02aff1529e84
lasilvadev <mslarissasilva@gmail.com>
2023-04-19 18:44:39 -0300

    Finalized project
    Finished design and added javascript.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Scroll to top Button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function() {
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500)
+});
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
